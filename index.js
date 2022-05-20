ba5f919313b91415341a0dfa68fdaa790ebe9c57
21ciprian <2021ciprianoffice@gmail.com>
2022-05-21 00:42:10 +0100

    Created routes folder

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import mongoose from 'mongoose'
 
 const app = express()
 const PORT = process.env.PORT || 5050
@@ -6,6 +7,15 @@ const PORT = process.env.PORT || 5050
 app.get('/', (req, res) => {
 	res.send('hey')
 })
+const connect = async () => {
+	try {
+		await mongoose.connect(process.env.MONGO)
+		console.log(`MongoDB Connected`)
+	} catch (error) {
+		console.log('Mongoose connection error: ', error)
+	}
+}
 app.listen(PORT, () => {
+	connect()
 	console.log(`Server is running on port ${PORT}`)
 })
