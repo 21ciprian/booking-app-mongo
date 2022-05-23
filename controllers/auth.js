adaee9290d519fc11ebeb55f2a1ef4d3a38154a2
21ciprian <2021ciprianoffice@gmail.com>
2022-05-23 01:18:14 +0100

    Added bcrypt to hash user  password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,12 @@
+import bcrypt from 'bcryptjs'
 import User from '../models/User.js'
 export const register = async (req, res, next) => {
   try
   {
+    const salt = bcrypt.genSaltSync(10)
+    const hashPassword = bcrypt.hashSync(req.body.password, salt)
     const newUser = new User({
-      username: req.body.username, email: req.body.email, password: req.body.password
+      username: req.body.username, email: req.body.email, password: hashPassword
     })
     await newUser.save()
     res.status(201).json('User has been created')
