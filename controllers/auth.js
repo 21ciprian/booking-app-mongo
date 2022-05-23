0ba6eb812a142ed7a6f4d39e26d262417f00027b
21ciprian <2021ciprianoffice@gmail.com>
2022-05-23 01:43:54 +0100

    Created login function and imported it in authRouter

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 import bcrypt from 'bcryptjs'
 import User from '../models/User.js'
+import { createError } from '../utils/error.js'
 export const register = async (req, res, next) => {
   try
   {
@@ -10,6 +11,23 @@ export const register = async (req, res, next) => {
     })
     await newUser.save()
     res.status(201).json('User has been created')
+  } catch (error)
+  {
+    next(error)
+  }
+}
+export const login = async (req, res, next) => {
+  try
+  {
+    const user = await User.findOne({ username: req.body.username })
+    if (!user) return next(createError(400, 'User not found'))
+
+    const isCorrectPassword = await bcrypt.compare(req.body.password, user.password)
+    if (!isCorrectPassword) return next(createError(400, 'Wrong username or password'))
+    const { password, isAdmin, ...otherDetails } = user._doc
+    res.status(200).json({
+      ...otherDetails
+    })
   } catch (error)
   {
     next(error)
