3733322727ea1034aa0f9f10990c6b336a4bae2f
21ciprian <2021ciprianoffice@gmail.com>
2022-05-25 00:01:55 +0100

    Created checkauthentication endpoint to test the verifyToken function

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,11 @@
 import express from 'express'
 import { deleteUser, getUser, getUsers, updateUser } from '../controllers/user.js'
+import { verifyToken } from '../utils/verifyToken.js'
 
 const router = express.Router()
+router.get('/checkauthentication', verifyToken, (req, res, next) => {
+  res.send('You are authenticated')
+})
 router.put('/:id', updateUser)
 router.delete('/:id', deleteUser)
 router.get('/:id', getUser)
