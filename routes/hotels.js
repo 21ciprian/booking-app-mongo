32569610e5a48ff7c3d8c9c76e02568f12f1d1f4
21ciprian <2021ciprianoffice@gmail.com>
2022-05-25 00:37:25 +0100

    Added verifyUser and verifyIsAdmin middlewares to hotel and users endpoints

--- routes/hotels.js
+++ routes/hotels.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from '../controllers/hotel.js'
+import { verifyIsAdmin } from '../utils/verifyToken.js'
 
 const router = express.Router()
-router.post('/', createHotel)
-router.put('/:id', updateHotel)
-router.delete('/:id', deleteHotel)
+router.post('/', verifyIsAdmin, createHotel)
+router.put('/:id', verifyIsAdmin, updateHotel)
+router.delete('/:id', verifyIsAdmin, deleteHotel)
 router.get('/:id', getHotel)
 router.get('/', getHotels)
 
-export default router
+export default router 
--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,20 @@
 import express from 'express'
 import { deleteUser, getUser, getUsers, updateUser } from '../controllers/user.js'
-import { verifyIsAdmin, verifyToken, verifyUser } from '../utils/verifyToken.js'
+import { verifyIsAdmin, verifyUser } from '../utils/verifyToken.js'
 
 const router = express.Router()
-router.get('/checkauthentication', verifyToken, (req, res, next) => {
-  res.send('You are authenticated')
-})
-router.get('/checkuser/:id', verifyUser, (req, res, next) => {
-  res.send('You are authenticated and you can delete your account')
-})
-router.get('/checkadmin/:id', verifyIsAdmin, (req, res, next) => {
-  res.send('You are authenticated and you can delete all accounts')
-})
-router.put('/:id', updateUser)
-router.delete('/:id', deleteUser)
-router.get('/:id', getUser)
-router.get('/', getUsers)
+// router.get('/checkauthentication', verifyToken, (req, res, next) => {
+//   res.send('You are authenticated')
+// })
+// router.get('/checkuser/:id', verifyUser, (req, res, next) => {
+//   res.send('You are authenticated and you can delete your account')
+// })
+// router.get('/checkadmin/:id', verifyIsAdmin, (req, res, next) => {
+//   res.send('You are authenticated and you can delete all accounts')
+// })
+router.put('/:id', verifyUser, updateUser)
+router.delete('/:id', verifyUser, deleteUser)
+router.get('/:id', verifyUser, getUser)
+router.get('/', verifyIsAdmin, getUsers)
 
 export default router
