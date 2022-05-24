c476cff5d0c1f54ac4af5498d819c4b7439fd8a3
21ciprian <2021ciprianoffice@gmail.com>
2022-05-25 00:19:57 +0100

    Created checkuser endpoint to verify if the user is logged in

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import { deleteUser, getUser, getUsers, updateUser } from '../controllers/user.js'
-import { verifyToken } from '../utils/verifyToken.js'
+import { verifyToken, verifyUser } from '../utils/verifyToken.js'
 
 const router = express.Router()
 router.get('/checkauthentication', verifyToken, (req, res, next) => {
   res.send('You are authenticated')
+})
+router.get('/checkuser/:id', verifyUser, (req, res, next) => {
+  res.send('You are authenticated and you can delete your account')
 })
 router.put('/:id', updateUser)
 router.delete('/:id', deleteUser)
