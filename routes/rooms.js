20876c081fea6178330fcddffa9cb41d282ae834
21ciprian <2021ciprianoffice@gmail.com>
2022-05-25 01:06:37 +0100

    Created rooms endpoints and imported rooms functions from room controller

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,5 +1,12 @@
 import express from 'express'
+import { createRoom, deleteRoom, getRoom, getRooms, updateRoom } from '../controllers/room.js'
+import { verifyIsAdmin } from '../utils/verifyToken.js'
 
 const router = express.Router()
+router.post('/:hotelId', verifyIsAdmin, createRoom)
+router.put('/:id', verifyIsAdmin, updateRoom)
+router.delete('/:id', verifyIsAdmin, deleteRoom)
+router.get('/:id', getRoom)
+router.get('/', getRooms)
 
-export default router
+export default router 
