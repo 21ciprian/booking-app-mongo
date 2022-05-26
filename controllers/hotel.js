db100a0d7e5ecb430792370a49ff9e8701e23a8c
21ciprian <2021ciprianoffice@gmail.com>
2022-05-26 02:10:58 +0100

    Created countBytype function and endpoint

--- controllers/hotel.js
+++ controllers/hotel.js
@@ -63,3 +63,22 @@ export const countByCity = async (req, res, next) => {
     next(error)
   }
 }
+export const countByType = async (req, res, next) => {
+  try
+  {
+    const hotelCount = await Hotel.countDocuments({ type: 'hotel' })
+    const appartmentCount = await Hotel.countDocuments({ type: 'appartment' })
+    const resortCount = await Hotel.countDocuments({ type: 'resort' })
+    const villaCount = await Hotel.countDocuments({ type: 'villa' })
+    const cabinCount = await Hotel.countDocuments({ type: 'cabin' })
+
+    res.status(200).json([ { type: 'hotel', count: hotelCount },
+    { type: 'appartment', count: appartmentCount },
+    { type: 'resort', count: resortCount },
+    { type: 'villa', count: villaCount },
+    { type: 'cabin', count: cabinCount } ])
+  } catch (error)
+  {
+    next(error)
+  }
+}
--- routes/hotels.js
+++ routes/hotels.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { countByCity, createHotel, deleteHotel, getHotel, getHotels, updateHotel } from '../controllers/hotel.js'
+import { countByCity, countByType, createHotel, deleteHotel, getHotel, getHotels, updateHotel } from '../controllers/hotel.js'
 import { verifyIsAdmin } from '../utils/verifyToken.js'
 
 const router = express.Router()
@@ -9,6 +9,6 @@ router.delete('/:id', verifyIsAdmin, deleteHotel)
 router.get('/find/:id', getHotel)
 router.get('/', getHotels)
 router.get('/countbyCity', countByCity)
-router.get('/countByType', getHotels)
+router.get('/countByType', countByType)
 
 export default router 
