3a9af75996f0bbe4557214aa6b8469f9219f6641
21ciprian <2021ciprianoffice@gmail.com>
2022-06-03 21:25:38 +0100

    Created room availability  endpoint to update  the unavailable  rooms of the hotel

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { createRoom, deleteRoom, getRoom, getRooms, updateRoom } from '../controllers/room.js'
+import { createRoom, deleteRoom, getRoom, getRooms, updateRoom, updateRoomAvaiability } from '../controllers/room.js'
 import { verifyIsAdmin } from '../utils/verifyToken.js'
 
 const router = express.Router()
 router.post('/:hotelId', verifyIsAdmin, createRoom)
 router.put('/:id', verifyIsAdmin, updateRoom)
+router.put('/availability/:id', updateRoomAvaiability)
 router.delete('/:hotelId/:id', verifyIsAdmin, deleteRoom)
 router.get('/:id', getRoom)
 router.get('/', getRooms)
